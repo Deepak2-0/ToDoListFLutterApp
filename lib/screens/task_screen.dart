7015f58fbe0cdecb9aad29433d4f923363a1cfb8
Deepak2-0 <choudharydeepak938@gmail.com>
2021-08-27 15:21:37 +0530

    bottom part ui done for task screen

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:to_do_list/widgets/to_do_items.dart';
 
 class TaskScreen extends StatelessWidget {
   const TaskScreen({Key? key}) : super(key: key);
@@ -7,6 +8,18 @@ class TaskScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.lightBlueAccent;
+      }
+      return Colors.black;
+    }
+
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
       floatingActionButton: FloatingActionButton(
@@ -21,7 +34,7 @@ class TaskScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 60),
+            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -62,6 +75,17 @@ class TaskScreen extends StatelessWidget {
                   topLeft: Radius.circular(20),
                   topRight: Radius.circular(20),
                 ),
+              ),
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 40),
+                children: [
+                  ToDoItems(
+                      item: "By Milk", isChecked: true, isNotRequired: false),
+                  ToDoItems(
+                      item: "By Eggs", isChecked: false, isNotRequired: false),
+                  ToDoItems(
+                      item: "By Bread", isChecked: false, isNotRequired: true),
+                ],
               ),
             ),
           ),
